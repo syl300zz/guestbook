5185f4aa8dd4e9e5ee5380e926c1500a69dac3c7
Junwoong Cha <chawnsnd@gmail.com>
2019-12-24 19:19:48 +0900

    준웅 추가함
    ㅎㅎㅎㅎ

--- 38TH_MybatisExam/src/guestbook/ui/GuestbookMain.java
+++ 38TH_MybatisExam/src/guestbook/ui/GuestbookMain.java
@@ -5,5 +5,7 @@ public class GuestbookMain {
     public static void main(String[] args) {
         GuestbookUI ui = new GuestbookUI();
         System.out.println();
+        //준웅 추가 코드
+        System.out.println("이히히");
     }
 }
